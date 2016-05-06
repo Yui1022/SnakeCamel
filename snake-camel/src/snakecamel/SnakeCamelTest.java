bcdb6b32ea4da0f3290a2b0cccd9d00b91cacf98
g14918ky <g14918ky@s30432.tsuda.ac.jp>
2016-05-06 12:05:35 +0900

    てすと内容かいたよ

--- snake-camel/src/snakecamel/SnakeCamelTest.java
+++ snake-camel/src/snakecamel/SnakeCamelTest.java
@@ -8,6 +8,19 @@ public class SnakeCamelTest {
 
 	@Test
 	public void test() {
+		SnakeCamelUtil snacam = new SnakeCamelUtil();
+		String expected = "SodaIceCream";
+		String actual = snacam.snakeToCamelcase("soda_ice_cream");
+		assertEquals(actual, expected);
+		
+		SnakeCamelUtil snacam2 = new SnakeCamelUtil();
+		String expected2 = "melon_cream_soda";
+		String actual2 = snacam.snakeToCamelcase("MelonCreamSoda");
+		assertEquals(actual2, expected2);
+			
+		
+		
+		
 		
 		fail("Not yet implemented");
 	}
