6121442a9d62c840875eef9cd96bca1d22100bb3
g14918ky <g14918ky@s30432.tsuda.ac.jp>
2016-05-06 10:58:34 +0900

    あ

---
+++ snake-camel/src/snakecamel/SnakeCamelTest.java
@@ -0,0 +1,15 @@
+package snakecamel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakeCamelTest {
+
+	@Test
+	public void test() {
+		
+		fail("Not yet implemented");
+	}
+
+}
