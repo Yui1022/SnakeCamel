e4e438ba36c3d2a6ac0896f8d90dd9806f648019
g14918ky <g14918ky@m39907.tsuda.ac.jp>
2016-05-12 14:56:02 +0900

    家でやるよ

--- snake-camel/src/snakecamel/SnakeCamelTest.java
+++ snake-camel/src/snakecamel/SnakeCamelTest.java
@@ -5,18 +5,35 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class SnakeCamelTest {
+	
 
 	@Test
-	public void test() {
-		SnakeCamelUtil snacam = new SnakeCamelUtil();
+	public void testSnaketoCamel() {
 		String expected = "SodaIceCream";
-		String actual = snacam.snakeToCamelcase("soda_ice_cream");
+		String actual = SnakeCamelUtil.snakeToCamelcase("soda_ice_cream");
+		assertEquals(expected,actual);
+	}
+		public void testCameltoSnake(){
+			
+		String expected = "SodaIceCream";
+		String actual = SnakeCamelUtil.snakeToCamelcase("soda_ice_cream");
 		assertEquals(actual, expected);
+		}
+		public void capitalizeTest(){
 		
-		SnakeCamelUtil snacam2 = new SnakeCamelUtil();
-		String expected2 = "melon_cream_soda";
-		String actual2 = snacam.snakeToCamelcase("MelonCreamSoda");
-		assertEquals(actual2, expected2);
+			String expected = "SWEET";
+			
+			String capSt = SnakeCamelUtil.capitalize("sweet");
+			assertEquals(capSt, expected);
+			
+		}
+	
+		
+
+				
+		/*String expected2 = "melon_cream_soda";
+		String actual2 = SnakeCamelUtil.camelToSnakecase("MelonCreamSoda");
+		assertEquals(actual2, expected2);*/
 			
 		
 		
@@ -24,5 +41,6 @@ public class SnakeCamelTest {
 		
 		fail("Not yet implemented");
 	}
+	}
 
-}
+
